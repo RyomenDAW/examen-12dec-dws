22fc678aba19edb051742395f0bb79e0752b66de
Israel Hernandez <israel.hernandez.chacon.al@iespoligonosur.org>
2024-12-12 02:53:11 +0100

    Todo preparado para el examen jokes aside

---
+++ examen/static/js/script.js
@@ -0,0 +1,10 @@
+alert(" Sea bienvenido al examen, disfrute de su experiencia =) ")
+
+function eliminar(){
+    var x = confirm("Eliminar lo que quieres eliminar, u sure?")
+    if (x)
+        return true;
+    else
+        return false;
+}
+
